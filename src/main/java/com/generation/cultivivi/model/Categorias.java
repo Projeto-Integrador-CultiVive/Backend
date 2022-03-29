cb1112aedf8f7320d9f52ddd3108c21f7feaab08
miguel <miguelvr1202@gmail.com>
2022-03-29 13:58:19 -0300

    Adicionando a Model categorias

---
+++ src/main/java/com/generation/cultivivi/model/Categorias.java
@@ -0,0 +1,38 @@
+package com.generation.cultivivi.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_categorias")
+public class Categorias {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "O atributo texto é obrigatório!")
+	@Size(min = 3, max = 100, message = "O atributo tipo deve possuir no min 3 e no max 100 caracteres")
+	private String tipo;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+}
