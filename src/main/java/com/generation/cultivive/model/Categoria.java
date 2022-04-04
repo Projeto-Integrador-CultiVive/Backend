e6e21b1ed58855368cf81fd5e1a1dd68ad99b30e
JoaoPedro <joaopedroflauzinonascimento@gmail.com>
2022-04-04 14:21:08 -0300

    CRUD - Parte 05

--- src/main/java/com/generation/cultivive/model/Categoria.java
+++ src/main/java/com/generation/cultivive/model/Categoria.java
@@ -1,9 +1,13 @@
 package com.generation.cultivive.model;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
@@ -11,16 +15,27 @@ import javax.validation.constraints.Size;
 @Entity
 @Table(name = "tb_categorias")
 public class Categoria {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	@NotBlank(message = "O atributo texto é obrigatório!")
 	@Size(min = 3, max = 100, message = "O atributo tipo deve possuir no min 3 e no max 100 caracteres")
 	private String tipo;
-	
+
 	private String descricao;
+
+	@OneToMany(mappedBy = "categoria", cascade = CascadeType.ALL)
+	private List<Produto> produto;
+
+	public List<Produto> getProduto() {
+		return produto;
+	}
+
+	public void setProduto(List<Produto> produto) {
+		this.produto = produto;
+	}
 
 	public String getDescricao() {
 		return descricao;
